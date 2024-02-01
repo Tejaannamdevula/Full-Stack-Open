a057e49e23bfe1aa428279894eb6aca1e91ff097
Teja Annamdevula <tejaannamdevula@gmail.com>
2024-02-01 18:32:32 +0530

    feat()Completed exercise 1.3 and 1.4

--- part-1/courseInfo/src/App.jsx
+++ part-1/courseInfo/src/App.jsx
@@ -29,20 +29,28 @@ const Total = (props)=>{
 
 
 const App =()=>{
-  const course = "Half Stack application devlopment"
-  const part1 = "Fundamentals of React"
-  const exercises1  = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const course ={
+    name:"Half Stack application devlopment",
+    parts : [
+      {
+        name:"Fundamentals of React",
+        exercises: 10
+      },{
+        name:"Using props to pass data",
+        exercises: 7
+      },
+      {
+        name:"State of a componen",
+        exercises: 14
+      }
+    ]
+  } 
 
   return (
     <div>
       <Header course = {course}/>
-      <Content part1 ={part1} part2 ={part2} part3 = {part3}
-        exercises1 ={exercises1} exercises2 ={exercises2} exercises3 ={exercises3} />
-      <Total exercises1 ={exercises1} exercises2 ={exercises2} exercises3 ={exercises3}></Total>
+      <Content parts ={parts} />
+      <Total parts = {parts}></Total>
     </div>
   )
 }
